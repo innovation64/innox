6f48d4a354fa5021cbe2cc364bc92406fa8ea98e
李洋 <liyang19991126@126.com>
2021-12-07 22:16:25 +0800

    up

--- AIOT/AIOTproject1/configure/config.sh
+++ AIOT/AIOTproject1/configure/config.sh
@@ -3,14 +3,16 @@ sudo apt-get upgrade
 sudo apt-get install libatlas-base-dev
 sudo apt install python3-venv
 sleep(1)
+mkdir /opt/test
+cd /opt/test
+sudo python3 -m venv env
 python3 -m venv my-project-env
 sleep(1)
-source my-project-env/bin/activate
+source env/bin/activate
 
 
 
 sudo pip3 install pyaudio
 sudo pip3 install scipy
-sudo pip3 install tqdm
 sudo pip3 install pixel-ring
 sudo pip3 install gpiozero
